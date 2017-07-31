3773b57e12092a93a92a1644924ecf1fc9aac618
Stephen Koo <stephen.theking@gmail.com>
2017-07-31 17:00:45 -0600

    Connect with Firebase

--- web/js/app.js
+++ web/js/app.js
@@ -1,16 +1,24 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCJoxhsDXOZKUnr4WkqQPw9iEI-kmOGhpA",
+  authDomain: "trails-across-new-mexico.firebaseapp.com",
+  databaseURL: "https://trails-across-new-mexico.firebaseio.com",
+  projectId: "trails-across-new-mexico",
+  storageBucket: "trails-across-new-mexico.appspot.com",
+  messagingSenderId: "1051009422594"
+};
+firebaseApp = firebase.initializeApp(config);
+var db = firebaseApp.database()
+
+vm = new Vue({
+  el: '#app',
+  firebase: {
+    trails: db.ref("/trails")
+  }
+})
+
 Vue.component('trail-listing', {
   props: ['trail'],
   template: '#trail-listing-tpl',
-})
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    trails: [
-      { id: 0, name: 'Library Loop Trail', distance: '0.5 mile', difficulty: 'Easy' },
-      { id: 1, name: 'South U.S. 550 Sidewalks', distance: '0.6 mile', difficulty: 'Easy' },
-      { id: 2, name: 'Village Office to Saveway Market Loop', distance: '0.8 mile', difficulty: 'Easy' },
-    ]
-  }
 })
 
