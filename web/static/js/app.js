29ebf29646023606acfa33229b72b7b3f58eaa23
Stephen Koo <stephen.theking@gmail.com>
2017-08-04 12:59:24 -0600

    Move firebase stub down

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,17 +1,3 @@
-// Initialize Firebase
-/*  not using firebase for now
-var config = {
-  apiKey: "AIzaSyCJoxhsDXOZKUnr4WkqQPw9iEI-kmOGhpA",
-  authDomain: "trails-across-new-mexico.firebaseapp.com",
-  databaseURL: "https://trails-across-new-mexico.firebaseio.com",
-  projectId: "trails-across-new-mexico",
-  storageBucket: "trails-across-new-mexico.appspot.com",
-  messagingSenderId: "1051009422594"
-};
-firebaseApp = firebase.initializeApp(config);
-var db = firebaseApp.database()
-*/
-
 vm = new Vue({
   el: '#app',
   data: {
@@ -29,3 +15,19 @@ Vue.component('trail-listing', {
   props: ['trail'],
   template: '#trail-listing-tpl',
 })
+
+
+
+// Initialize Firebase
+/*  not using firebase for now
+var config = {
+  apiKey: "AIzaSyCJoxhsDXOZKUnr4WkqQPw9iEI-kmOGhpA",
+  authDomain: "trails-across-new-mexico.firebaseapp.com",
+  databaseURL: "https://trails-across-new-mexico.firebaseio.com",
+  projectId: "trails-across-new-mexico",
+  storageBucket: "trails-across-new-mexico.appspot.com",
+  messagingSenderId: "1051009422594"
+};
+firebaseApp = firebase.initializeApp(config);
+var db = firebaseApp.database()
+*/
