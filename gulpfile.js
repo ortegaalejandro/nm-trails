988779fde2529035541b066f46000cc132544977
Stephen Koo <stephen.theking@gmail.com>
2017-08-07 11:50:26 -0600

    Added some new components

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 
+// This task generates a new service worker script
 gulp.task('generate-service-worker', function(callback) {
   var swPrecache = require('sw-precache');
   var rootDir = 'web';
 
   swPrecache.write(`${rootDir}/service-worker.js`, {
-    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    // We cache all static files in the web directory with these extensions
+    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,jpeg,JPG,gif,svg,eot,ttf,woff}'],
     stripPrefix: rootDir,
     verbose: true,
 
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,3 +1,29 @@
+// Define a new component called <trail-listing>
+Vue.component('trail-list', {
+  // Takes an array of trails as a property
+  props: ['trails'],
+
+  // Use the template defined with the id="trail-list-tpl"
+  template: '#trail-list-tpl',
+})
+
+Vue.component('trail-listing', {
+  // Takes one object named "trail" as a property
+  props: ['trail'],
+
+  // Use the template defined with the id="trail-listing-tpl"
+  template: '#trail-listing-tpl',
+})
+
+Vue.component('trail-details', {
+  // Takes one object named "trail" as a property
+  props: ['trail'],
+
+  // Use the template defined with the id="trail-details-tpl"
+  template: '#trail-details-tpl',
+})
+
+
 // vm stands for "view-model"
 vm = new Vue({
   // Bind this view-model to the element with id="app"
@@ -6,6 +32,7 @@ vm = new Vue({
   // Declare what data is in this view-model
   data: {
     trails: null,
+    currentView: 'trail-list',
   },
 
   // This gets executed when this view-model is created
@@ -16,18 +43,8 @@ vm = new Vue({
       self.trails = data;
       console.log(data);
     });
-  }
+  },
 })
-
-// Define a new component called <trail-listing>
-Vue.component('trail-listing', {
-  // Takes one object named "trail" as a property
-  props: ['trail'],
-
-  // Use the template defined with the id="trail-listing-tpl"
-  template: '#trail-listing-tpl',
-})
-
 
 
 // Initialize Firebase
