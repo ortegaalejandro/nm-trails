7e8cd708f387f3d480ce1833b545bf967111ead9
Stephen Koo <stephen.theking@gmail.com>
2017-08-10 07:37:46 -0600

    Add trail details page

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,26 +1,68 @@
 // Define a new component called <trail-listing>
-Vue.component('trail-list', {
+var trailList = Vue.component('trail-list', {
   // Takes an array of trails as a property
-  props: ['trails'],
+  // props: ['trails'],
 
   // Use the template defined with the id="trail-list-tpl"
   template: '#trail-list-tpl',
+
+  data: function() {
+    return {
+      trails: null,
+    }
+  },
+
+  // This gets executed when this view-model is created
+  created: function() {
+    // Load the data from our static JSON files
+    $.getJSON('/static/data/trails.json', function (data) {
+      this.trails = data;
+      console.log(data);
+    }.bind(this));
+  },
 })
 
 Vue.component('trail-listing', {
   // Takes one object named "trail" as a property
-  props: ['trail'],
+  props: ['trail', 'trailId'],
 
   // Use the template defined with the id="trail-listing-tpl"
   template: '#trail-listing-tpl',
 })
 
-Vue.component('trail-details', {
+var trailDetails = Vue.component('trail-details', {
   // Takes one object named "trail" as a property
-  props: ['trail'],
+  props: ['id'],//['trail'],
 
   // Use the template defined with the id="trail-details-tpl"
   template: '#trail-details-tpl',
+
+  data: function() {
+    return {
+      trail: null,
+      loading: false,
+    }
+  },
+
+  // This gets executed when this view-model is created
+  created: function() {
+    this.loading = true;
+    // Load the data from our static JSON files
+    $.getJSON('/static/data/trails.json', function (data) {
+      this.trail = data[this.id];
+      this.loading = false;
+    }.bind(this));
+  },
+})
+
+
+// Define the routes
+var router = new VueRouter({
+  routes: [
+    { path: '/trails', component: trailList},
+    { path: '/trails/:id', component: trailDetails, props: true},
+    { path: '/', redirect: '/trails' }
+  ]
 })
 
 
@@ -29,34 +71,6 @@ vm = new Vue({
   // Bind this view-model to the element with id="app"
   el: '#app',
 
-  // Declare what data is in this view-model
-  data: {
-    trails: null,
-    currentView: 'trail-list',
-  },
-
-  // This gets executed when this view-model is created
-  created: function() {
-    // Load the data from our static JSON files
-    var self = this;
-    $.getJSON('/static/data/trails.json', function (data) {
-      self.trails = data;
-      console.log(data);
-    });
-  },
+  // "Inject" the router we defined above into the application
+  router: router,
 })
-
-
-// Initialize Firebase
-/*  not using firebase for now
-var config = {
-  apiKey: "AIzaSyCJoxhsDXOZKUnr4WkqQPw9iEI-kmOGhpA",
-  authDomain: "trails-across-new-mexico.firebaseapp.com",
-  databaseURL: "https://trails-across-new-mexico.firebaseio.com",
-  projectId: "trails-across-new-mexico",
-  storageBucket: "trails-across-new-mexico.appspot.com",
-  messagingSenderId: "1051009422594"
-};
-firebaseApp = firebase.initializeApp(config);
-var db = firebaseApp.database()
-*/
